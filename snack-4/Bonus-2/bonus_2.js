5082648d684ca3115db4b21fcce5acc998ff586c
Raff_12 <raffelecoppola381@gmail.com>
2025-03-19 18:57:46 +0100

    feat: esercitazione sugli oggetti

--- snack-4/Bonus-2/bonus_2.js
+++ snack-4/Bonus-2/bonus_2.js
@@ -20,6 +20,125 @@ btn_min.addEventListener("click", ()=>{
             par_graf.innerHTML = `<b>Contatore: </b> ${count}`;
         }, 1000);
     }
-    console.log(count);
     return count;
 })
+
+// replicare lo stesso procedimento con il max creando uno oggetto:
+
+// La funzione restituisce un selettore css valido dai passargli gli elementi, se esistono. 
+function elementquery(element) {
+    return document.querySelector(element) || null;
+}
+
+class ContMax {
+    constructor(id, count) {
+        this.count = count; // valore di indice 
+        this.element = elementquery(id); //elemento padre corrispondente con tutti i suoi figli etichettati.
+        this.btn_max = document.getElementById("btn-max"); // Bottone di incremento.
+        this.par_max = document.getElementById("par-id-1"); // indice di contatore nella rappresentazione di testo.
+
+        this.BtnEvent = this.BtnEvent.bind(this); 
+        // Per fare in modo che la funzione della classe interagisca correttamente con l'HTML,
+        // è necessario usare il metodo bind(). Questo assicura che "this" faccia riferimento
+        // all'istanza della classe, permettendoci di accedere correttamente agli elementi
+        // e completare l'evento senza errori. (elementi nominati nella classe);
+        this.btn_max.addEventListener("click", this.BtnEvent); // chiamata del evento in funzione.
+    }
+
+    // funzione di incremento con la stessa funzione di implementagio fatta in precedenza.
+    BtnEvent() {
+        if (this.count < 10) {
+            this.count++;
+            this.par_max.innerHTML = `<b>Contatore:</b> ${this.count}`;
+        } else{
+            this.btn_max.setAttribute("disabled", "disabled");
+            this.par_max.innerHTML = `<b>Contatore:</b> ${this.count}`;
+            setTimeout(()=>{
+                this.btn_max.removeAttribute("disabled");
+                this.count = 0;
+                this.par_max.innerHTML = `<b>Contatore:</b> ${this.count}`;
+            }, 1000);
+        }
+    }
+}
+
+const count_1 = new ContMax(".box-cont", 0);
+
+// scopri parola nascosa attraveso il box
+// replica del esercizio fatto di sopra:
+
+class ShowPar {
+    constructor(frase, element){
+        this.frase = frase;
+        this.element = element;
+        this.list = ["Hello World!!", "Ciao mondo!!", "Ciao, è grazie per avermi cliccato.", "Hai rotto le scatole, non cliccarmi più!!"];
+        this.btn = document.getElementById("btn-show");
+        this.par = document.getElementById("par-show");
+
+        this.Show = this.Show.bind(this);
+        this.btn.addEventListener("click", this.Show);
+    }
+
+    Show(){
+        this.btn.setAttribute("disabled", "disabled");
+        return this.par.innerHTML = `<b>${this.frase}</b>`;
+    }
+
+   Random(){
+        let this_par = this.list[Math.floor(Math.random() * this.list.length)];
+        return this.frase = this_par;
+    }
+}
+
+const show_par = new ShowPar("Corriere della Sera", `${document.querySelector(".container-ex" ? ".container-ex" : null)}`);
+show_par.Random();
+
+// Aggiungi una lista di paragrafi con le classi:
+
+class Add_Par{
+    constructor(parent){
+        this.parent = document.querySelector(parent);
+        this.input = document.getElementById("text");
+        this.btn_add = document.getElementById("add-btn");
+        this.InputCheck = this.InputCheck.bind(this);
+        this.color = document.getElementById("color-inp");
+        this.btn_add.addEventListener("click", this.InputCheck);
+    }
+
+    InputCheck(){
+        let value = this.input.value;
+        // console.log(value);
+        let value_color = this.color.value;
+        // console.log(value_color)
+        if (value !== "") {
+            let pargrafo_box = document.createElement("p");
+            pargrafo_box.className = "par-box";
+            pargrafo_box.style.backgroundColor = value_color;
+            let text_node = document.createTextNode(value);
+            pargrafo_box.append(text_node);
+            this.parent.appendChild(pargrafo_box);
+            return this.parent
+        } else{
+            window.alert("Inserisci come qualcosa di input");
+        }
+        }
+}
+
+class reset extends Add_Par{
+    constructor(parent){
+        super(parent);
+        this.reset = document.getElementById("reset-btn");
+        this.InputReset = this.InputReset.bind(this);
+        this.reset.addEventListener("click", this.InputReset);
+    }
+
+    InputReset(){
+        // return console.log("hello");
+        this.input.value = "";  // this.input viene inizializzato dopo
+        this.parent.innerHTML = "";
+        window.alert("La lista è stata resettata.")
+    }
+}
+
+const parent_node_list = new Add_Par(".par-list");
+const reset_Btn = new reset(".par-list");
